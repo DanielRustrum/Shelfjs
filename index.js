d20883c276d9fb795db6f8df6392d119eb07b43c
Daniel Rustrum <DanielRustrum@users.noreply.github.com>
2023-06-27 06:06:09 +0000

    changes

---
+++ index.js
@@ -0,0 +1,6 @@
+function App() {
+
+    return shelf.component(`<h1>Hello!</h1>`, [sig])
+}
+
+shelf.render(App, "app")
--- src/shelf.js
+++ src/shelf.js
@@ -1,5 +1,23 @@
 const shelf = {
-    component: () => {},
-    signals: () => {},
-    render: () => {}
+    component: (string) => {
+        return {
+            scope: (variables) => {},
+            compile: () => {
+                return string
+            }
+        }
+    },
+    signal: (default_value = undefined) => {
+        let value = default_value
+        return (value)
+    },
+    render: (component, root) => {
+        let ready_component = component().compile()
+        let root_el = document.querySelector(root)
+        root_el.innerHTML = ready_component
+
+        // for (const element of root_el) {
+        //     element.innerHTML = ready_component
+        // }
+    }
 }
